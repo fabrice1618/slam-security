5ff6f2b1ec0efd697676d3153b768ae1a7635ade
ZOLTOWSKI Valérian <valerian.zoltowski@gmail.com>
2021-10-06 16:47:56 +0200

    add exceptions and loginException

--- controller/AuthController.php
+++ controller/AuthController.php
@@ -1,13 +1,19 @@
 <?php
-declare(strict_types=1);
+//declare(strict_types=1);
 
 class AuthController
 {
+    /**
+     * @throws LoginException
+     */
     public function login()
     {
         $username = ParamUtils::findPOSTParam('username');
         $password = ParamUtils::findPOSTParam('password');
-
+        if(empty($username) || empty($password))
+        {
+            throw new LoginException("Empty Credentials");
+        }
     }
 
     public function logout()
