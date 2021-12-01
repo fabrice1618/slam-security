ef1717d7e6c57074ea62f3ea88a677980da7cbf2
unknown <robin.peignet.contact@gmail.com>
2021-12-01 14:32:00 +0100

    update database

--- webserver/class/Database.php
+++ webserver/class/Database.php
@@ -1,5 +1,26 @@
 <?php
 
-//"Faire plus l'inverse que le contraire"
-// Faire l'ouverture de la base de données
+class Database
+{
+    require_once("DbConfig.php");
+    private dbHost = DbConfig::__get(DBCONFIG_HOST);
+    private dbName = DbConfig::__get(DBCONFIG_DBNAME);
+    private dbCharset = DbConfig::__get(DBCONFIG_CHARSET);
+    private dbUser = DbConfig::__get(DBCONFIG_USER);
+    private dbPassWorld = DbConfig::__get(DBCONFIG_PASSWORD);
 
+    private function connect($host, $dbName, $User, $passWorld){
+        try
+        {
+            $db = new PDO('mysql:host='.$host.';dbname='.$dbName.';charset=utf8', $User, $passWorld);
+        }
+        catch (Exception $e)
+        {
+                die('Erreur : ' . $e->getMessage());
+        }
+    }
+
+    public function getConnection(){
+        return $db;
+    }
+}
