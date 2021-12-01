0432fc771f91c15e33f0c37a1c6e774e87fd47da
unknown <robin.peignet.contact@gmail.com>
2021-12-01 13:34:49 +0100

    notFound update

--- webserver/controller/HomeController.php
+++ webserver/controller/HomeController.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once("ViewManager.php");
+
 class HomeController implements Controller
 {
     /**
--- webserver/controller/NotFoundController.php
+++ webserver/controller/NotFoundController.php
@@ -1,14 +1,15 @@
 <?php
 
+require_once("ViewManager.php");
+
 class NotFoundController implements Controller
 {
     /**
      * @throws Exception
      */
-    public function loadView()
+    public function loadView(string $pageNoteFound)
     {
-
-        ViewManager::view("notFound-template", []);
+        ViewManager::view("notFound-template", ["PAGE_NOT_FOUND" => $pageNoteFound]);
     }
 
     public function default()
