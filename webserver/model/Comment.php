9c81def81e5d4afe051edef7940684c36da138c0
ZOLTOWSKI Valérian <valerian.zoltowski@gmail.com>
2022-02-09 14:17:06 +0100

    fix comment model

--- webserver/model/Comment.php
+++ webserver/model/Comment.php
@@ -1,31 +1,120 @@
 <?php
 
-class Comment extends Model{
-    
-    private mixed $title;
-    private mixed $message;
-    private mixed $user;
+declare(strict_types=1);
+const QUERY_INSERT = "INSERT INTO comment (id, username, comment,date) VALUES (NULL, :username, :comment,:date) ";
+const QUERY_DELETE = "DELETE FROM comment WHERE id = :id ";
+const QUERY_INDEX = "SELECT * FROM comment ";
 
 
-    public function getTitle()
-    {
-        return ($this->title);
-    }
-    public function getMessage()
-    {
-        return ($this->message);
-    }
-    public function getUser()
-    {
-        return ($this->user);
-    }
-    public function
+class Comment extends Model
+{
+    private int $id;
+    private string $username;
+    private string $content;
+    private DateTime $date;
 
-    public function getUserFromBdd(){
-        $sql =  'SELECT Title, message, user FROM comments ORDER BY title';
-        foreach  ($conn->query($sql) as $row) {
-            echo($row['title']);
+    public function create(): void
+    {
+        $stmt1 = $this->PDO->prepare(QUERY_INSERT);
+        $stmt1->bindValue(':username', $this->username);
+        $stmt1->bindValue(':comment', $this->content);
+        $stmt1->bindValue(':date', $this->date->format('Y-m-d H:i:s'), PDO::PARAM_STR);
+        if ($stmt1->execute()) {
+            $this->id = (int)$this->pdo->lastInsertId();
         }
     }
-    
+
+
+    public function read($limit): array
+    {
+        $comments = array();
+        $stmt1 = $this->PDO->prepare(QUERY_INDEX . $limit);
+        if ($stmt1->execute()) {
+            $rows = $stmt1->fetchAll();
+            foreach ($rows as $row) {
+                $comment = new Comment();
+                $comment->setId($row["id"]);
+                $comment->setUsername($row["username"]);
+                $comment->setContent($row["comment"]);
+                $comment->setDate(DateTime::createFromFormat('Y-m-d H:i:s', $row["date"]));
+                array_push($comments, $comment);
+            }
+        }
+        return $comments;
+    }
+
+    public function delete()
+    {
+        $stmt1 = $this->PDO->prepare(QUERY_DELETE);
+        $stmt1->bindValue(':id', $this->id, PDO::PARAM_INT);
+        if ($stmt1->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDate(): DateTime
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param DateTime $date
+     */
+    public function setDate(DateTime $date): void
+    {
+        $this->date = $date;
+    }
 }
