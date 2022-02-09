ec499e387b2385c2f898a5ae2f0b870e8d9ed87b
Maurin Célian <celianmaurin@hotmail.com>
2022-02-09 14:02:41 +0100

    Coucou valoche <3 <3

--- webserver/model/Comment.php
+++ webserver/model/Comment.php
@@ -1,5 +1,31 @@
 <?php
-declare(strict_types = 1);
-class Comment{
-    &
+
+class Comment extends Model{
+    
+    private mixed $title;
+    private mixed $message;
+    private mixed $user;
+
+
+    public function getTitle()
+    {
+        return ($this->title);
+    }
+    public function getMessage()
+    {
+        return ($this->message);
+    }
+    public function getUser()
+    {
+        return ($this->user);
+    }
+    public function
+
+    public function getUserFromBdd(){
+        $sql =  'SELECT Title, message, user FROM comments ORDER BY title';
+        foreach  ($conn->query($sql) as $row) {
+            echo($row['title']);
+        }
+    }
+    
 }
--- webserver/model/model.php
+++ webserver/model/model.php
@@ -2,98 +2,14 @@
 declare(strict_types = 1);
 class Model
 {
-    private $table_definition = null;
-    private String $dbh;
+
+    private PDO $PDO;
 
     protected $data = [];
 
-    public function __construct( $aTableDefinition ) :
+    public function __construct()
     {
-        $this->table_definition = $this->cleanTableDefinition($aTableDefinition);
-
-        $this->setDefault();
-
-        $this->dbh = Database::connexion();
+        $this->PDO = Database::Connect();
     }
-
-    public function setDefault()
-    {
-        foreach ($this->table_definition as $sNomChamp => $aChamp) {
-            $this->data[$sNomChamp] = $aChamp['default'];
-        }
-    }
-
-    public function __get($sName)
-    {
-        if (! array_key_exists($sName, $this->table_definition )) {
-            throw new \Exception(__CLASS__.": undefined property $sName", 1);
-        }
-  
-        return($this->data[$sName]);
-    }
-  
-    public function __set( $name, $value )
-    {
-        if ( ! array_key_exists($name, $this->table_definition) ) {
-            throw new Exception(__CLASS__.": Le champ $name n'existe pas dans l'objet", 1);
-        }
-
-        if ( ! $this->validate( $name, $value ) ) {
-            throw new Exception(__CLASS__.": Erreur mise à jour champ $name avec $value. Valeur invalide", 1);    
-        }
-
-        $this->data[$name] =  $value;        
-    }
-
-    public function validate( $name, $value )
-    {
-//        var_dump($this->table_definition);
-        $ValidFunction = $this->table_definition[$name]['valid'];
-        $lValid = $ValidFunction($value);
-
-        return($lValid);
-    }
-
-    public function toArray()
-    {
-        return($this->data);
-    }
-
-    public function toString()
-    {
-        return(json_encode($this->data));
-    }
-
-    protected function cleanTableDefinition($aInput)
-    {
-
-        if ( ! is_array($aInput) || count($aInput) == 0 ) {
-            throw new \Exception(__CLASS__.": table definition incorrecte", 1);
-        }
-
-        $aTableDefinition = array();
-
-        foreach ($aInput as $sChamp => $aParameters) {
-            
-            if ( isset($aParameters['valid']) && is_string($aParameters['valid']) && !empty($aParameters['valid']) ) {
-                $aParameters['valid'] === $aParameters['valid'];
-            } else {
-                $aParameters['valid'] = "Valid::alwaysTrue";
-            }
-
-            $aParameters['default'] = $aParameters['default'] ?? null;
-
-            if ( !isset($aParameters['pdo_type']) ) {
-                $aParameters['pdo_type'] = PDO::PARAM_STR;
-            }
-
-            // Parametres normalisés
-            $aTableDefinition[$sChamp] = $aParameters;                
-        }
-
-        return($aTableDefinition);
-    }
-
-    
 
 }
